af2219fa265b76c16cea0649718eaa7a0123e289
phozzy <arthur.fayzullin@gmail.com>
2020-02-07 22:01:57 +0100

    Implement convertion to piglatin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
+fn get_string() -> String {
+    loop {
+        let mut word = String::new();
+        println!("Please enter your word:");
+        io::stdin().read_line(&mut word)
+            .expect("Failed to read line");
+        break match word.trim().parse() {
+            Ok(str) => str,
+            Err(_) => {
+                println!("Wrong value! {}", word);
+                continue;
+            },
+        };
+    }
+}
+fn convert_string(word: &String) -> String {
+    let vowel = ['a', 'e', 'i', 'o', 'u'];
+    let mut counter: u32 = 0;
+    let mut result = String::new();
+    let mut ending = String::new();
+    for character in word.chars() {
+        counter += 1;
+        if counter == 1 {
+            if vowel.contains(&character) {
+                ending = String::from("hay");
+                result = character.to_string();
+            } else {
+                ending = character.to_string() + "ay";
+                result = "".to_string();
+            };
+        } else {
+            result += &character.to_string();
+        };
+    };
+    result + &ending
+}
 fn main() {
-    println!("Hello, world!");
+    let word = get_string();
+    println!("converted to: {}", convert_string(&word));
 }
